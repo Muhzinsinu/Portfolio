a6219570bd34944dbc23a3159b29259a5335f369
Muhzinsinu <107618326+Muhzinsinu@users.noreply.github.com>
2025-10-02 11:57:24 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -154,7 +154,7 @@ export default function Home() {
               ]}
             />
           </div>
-         <div className="space-y-12 max-w-3xl mx-auto mt-[15px]">
+         <div className="space-y-12 max-w-3xl mx-auto mt-[20px]">
             <ExperienceItem
               title="Web Developer"
               company="Up2 Tech Kunnamkulam"
